246b56116eff1627db57353454d12019c95ce447
lemonshushu <ifnt7luv@gmail.com>
2021-04-10 06:33:17 +0900

    fixed bug

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -19,41 +19,30 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-const unixTimestamp = (date) => {
-  return date.getTime() / 1000;
-}
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
 app.get('/api/timestamp', (req, res, next) => {
   const nowDate = new Date();
   res.json({
-    unix: unixTimestamp(nowDate),
+    unix: nowDate.getTime(),
     utc: nowDate.toUTCString()
   });
 });
 
-app.get('/api/timestamp/:date', (req, res, next) => {
+app.get('/api/timestamp/:date?', (req, res, next) => {
   const date = req.params.date;
-  let dateObject = new Date(date);
+  let dateObject = new Date(date).getTime() ? new Date(date) : new Date(Number(date));
   if (dateObject) {
     res.json({
-      unix: unixTimestamp(dateObject),
+      unix: dateObject.getTime(),
       utc: dateObject.toUTCString()
     });
   } else {
-    dateObject = new Date(date * 1000);
-    if (dateObject) {
-      res.json({
-        unix: date,
-        utc: dateObject.toUTCString()
-      });
-    } else {
-      res.json({error: 'Invalid Date'});
-    }
+    res.json({ error: 'Invalid Date' });
   }
 });
 
