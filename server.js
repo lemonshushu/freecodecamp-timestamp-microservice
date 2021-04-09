acf85896ad5fa03af213554df91420fcadeaffa4
lemonshushu <ifnt7luv@gmail.com>
2021-04-09 19:17:41 +0900

    finished

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // where your node app starts
 
 // init project
+require('dotenv').config();
 var express = require('express');
 var app = express();
 
@@ -18,10 +19,42 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+const unixTimestamp = (date) => {
+  return date.getTime() / 1000;
+}
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
+});
+
+app.get('/api/timestamp', (req, res, next) => {
+  const nowDate = new Date();
+  res.json({
+    unix: unixTimestamp(nowDate),
+    utc: nowDate.toUTCString()
+  });
+});
+
+app.get('/api/timestamp/:date', (req, res, next) => {
+  const date = req.params.date;
+  let dateObject = new Date(date);
+  if (dateObject) {
+    res.json({
+      unix: unixTimestamp(dateObject),
+      utc: dateObject.toUTCString()
+    });
+  } else {
+    dateObject = new Date(date * 1000);
+    if (dateObject) {
+      res.json({
+        unix: date,
+        utc: dateObject.toUTCString()
+      });
+    } else {
+      res.json({error: 'Invalid Date'});
+    }
+  }
 });
 
 
